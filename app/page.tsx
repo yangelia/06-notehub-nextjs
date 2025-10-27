c922783a39c85d561a730ef2be50708a3893f2a5
yangelia <yana.sychova@gmail.com>
2025-10-27 14:59:11 +0200

    fix styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,26 @@ import css from "./page.module.css";
 
 export default async function Home() {
   return (
-    <main className={css.container}>
-      <h1 className={css.title}>Welcome to NoteHub</h1>
-      <p className={css.description}>
-        NoteHub is a simple and efficient application designed for managing
-        personal notes. It helps keep your thoughts organized and accessible in
-        one place, whether you are at home or on the go.
-      </p>
-      <p className={css.description}>
-        The app provides a clean interface for writing, editing, and browsing
-        notes. With support for keyword search and structured organization,
-        NoteHub offers a streamlined experience for anyone who values clarity
-        and productivity.
-      </p>
-      <div style={{ textAlign: "center", marginTop: "2rem" }}>
-        <Link
-          href="/notes"
-          style={{
-            display: "inline-block",
-            padding: "12px 24px",
-            backgroundColor: "#0d6efd",
-            color: "white",
-            textDecoration: "none",
-            borderRadius: "4px",
-            fontSize: "16px",
-          }}
-        >
-          Go to Notes
-        </Link>
+    <main className={css.main}>
+      <div className={css.container}>
+        <h1 className={css.title}>Welcome to NoteHub</h1>
+        <p className={css.description}>
+          NoteHub is a simple and efficient application designed for managing
+          personal notes. It helps keep your thoughts organized and accessible
+          in one place, whether you are at home or on the go.
+        </p>
+        <p className={css.description}>
+          The app provides a clean interface for writing, editing, and browsing
+          notes. With support for keyword search and structured organization,
+          NoteHub offers a streamlined experience for anyone who values clarity
+          and productivity.
+        </p>
+
+        <div style={{ textAlign: "center", marginTop: "2rem" }}>
+          <Link href="/notes" className={css.addButton}>
+            Go to Notes
+          </Link>
+        </div>
       </div>
     </main>
   );
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: your name</p>
+          <p>Developer: Yana S</p>
           <p>
             Contact us:
-            <a href="mailto:student@notehub.app">student@notehub.app</a>
+            <a href="mailto:yana_s@notehub.app">yana_s@notehub.app</a>
           </p>
         </div>
       </div>
